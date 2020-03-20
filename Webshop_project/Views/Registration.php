59852f510e2c396dec2e0537c5172d6ddbd66f27
adamg94 <adamgulyas@outlook.com>
2020-03-20 16:36:51 +0100

    Registration.php labels fixed

--- Webshop_project/Views/Registration.php
+++ Webshop_project/Views/Registration.php
@@ -9,28 +9,28 @@
         <h2>Register</h2>
         <form action="Register.php" method="post">
             <div class="form-group">
-            <label>FirstName</label>
-            <input type="text" name="FirstName" class="form-control" required>
+            <label for="FirstName">FirstName</label>
+            <input id="FirstName" type="text" name="FirstName" class="form-control" required>
             </div>
             <div class="form-group">
-            <label>LastName</label>
-            <input type="text" name="LastName" class="form-control" required>
+            <label for="LastName">LastName</label>
+            <input id="LastName" type="text" name="LastName" class="form-control" required>
             </div>
             <div class="form-group">
-            <label>E-mail Address</label>
-            <input type="email" name="EmailAddress" class="form-control" required>
+            <label for="EmailAddress">E-mail Address</label>
+            <input id="EmailAddress" type="email" name="EmailAddress" class="form-control" required>
             </div>
             <div class="form-group">
-            <label>Password</label>
-            <input type="password" name="Password" class="form-control" required>
+            <label for="Password">Password</label>
+            <input id="Password" type="password" name="Password" class="form-control" required>
             </div>
             <div class="form-group">
-            <label>Address</label>
-            <input type="password" name="Address" class="form-control" required>
+            <label for="Address">Address</label>
+            <input id="Address"type="password" name="Address" class="form-control" required>
             </div>
-            <div class="form-group">
-            <label>PhoneNumber</label>
-            <input type="password" name="PhoneNumber" class="form-control" required>
+            <div clas s="form-group">
+            <label for="PhoneNumber">PhoneNumber</label>
+            <input id="PhoneNumber" type="password" name="PhoneNumber" class="form-control" required>
             </div>
             <button type="submit" class="btn btn-primary"> Register </button>
         </form>
