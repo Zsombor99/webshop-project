8d1030cd8523d4beac7b06b1e2ec06e676b413a3
adamg94 <adamgulyas@outlook.com>
2020-03-21 16:22:09 +0100

    AboutUs.php changed back to normal state

--- Webshop_project/Controllers/AboutUs.php
+++ Webshop_project/Controllers/AboutUs.php
@@ -2,12 +2,8 @@
 class AboutUs extends Controller{
     public static $Title = "About us";
     public static function test(){
-        $EmailAddress = "admin@webshop.hu";
-        $Password = "password";
         
-        //$User = self::query("SELECT `EmailAddress` FROM `User` WHERE `EmailAddress` = '?' AND `PasswordHash` = '?'", [$EmailAddress, $Password]);
-        print_r(self::query("SELECT `EmailAddress` FROM `User` WHERE `EmailAddress` = '?' AND `PasswordHash` = '?'", [$EmailAddress, $Password]));
-        //print_r(self::query("SELECT * FROM `User`"));
+        print_r(self::query("SELECT * FROM `User`"));
 
     }
     
