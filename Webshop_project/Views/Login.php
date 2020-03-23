5d75cb2073828bf6066fb83bd56330a4d2936e88
ungererik <ungererik11@gmail.com>
2020-03-23 14:57:19 +0100

    login css elkezdve

--- Webshop_project/Views/Login.php
+++ Webshop_project/Views/Login.php
@@ -1,16 +1,35 @@
+<style>
+.form-container {
+    border: 1px solid #ffffff;
+    padding: 50px 60px;
+    margin-top: 6vh;
+    -webkit-box-shadow: -1px 4px 26px 11px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: -1px 4px 26px 11px rgba(0, 0, 0, 0.75);
+    box-shadow: -1px 4px 26px 11px rgba(0, 0, 0, 0.75);
+    background-color: rgb(68, 43, 22)
+
+}
+.tx {
+    color: white;
+}
+
+</style>
+<div class="container-fluid bg">
+<h1 class="text-center l">Login</h2>
+<h6 class="text-center"> If you don't have an account, please register!</h5>
 <div class="row">
-    <div class="cold-md-6">
-        <h2>Login</h2>
-        <form action="<?php Login::_Login() ?>" method="post">
+<div class="col-md-4 col-sm-4 col-xs-12"></div>
+        <div class="col-md-4 col-sm-4 col-xs-12">
+        <form class="form-container" action="<?php Login::_Login() ?>" method="post">
             <div class="form-group">
-                <label for="EmailAddress">E-mail Address</label>
+                <label class="tx" for="EmailAddress">E-mail</label>
                 <input id="EmailAddress" type="email" name="EmailAddress" class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="Password">Password</label>
+                <label class="tx" for="Password">Password</label>
                 <input id="Password" type="password" name="Password" class="form-control" required>
             </div>
-            <button type="submit" class="btn btn-primary">Login</button>
+            <button type="submit" class="btn btn-success btn-block">Login</button>
         </form>
     </div>
 </div>
