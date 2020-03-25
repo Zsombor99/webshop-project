75af6c81433ad973007da73b1e0bdede4b9953c1
ZajaczAkos <55364288+ZajaczAkos@users.noreply.github.com>
2020-03-25 18:51:37 +0100

    footer maybe done

--- Webshop_project/Layout.php
+++ Webshop_project/Layout.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link href="assets/css/main.css" rel="stylesheet">
+    <link href="assets/css/footer.css" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <title><?= $Title ?></title>
@@ -46,16 +47,40 @@
         <?php include($Content) ?>
     </main>
 
-    <footer>
-        <div class="container-fluid">
-            Copyright &copy; Webshop <?= date("Y") ?><br>
-            <a href="https://www.google.com/intl/hu/policies/privacy/" target="_blank">Adatvédelem</a><span aria-hidden="true" role="presentation"> - </span><a href="https://www.google.com/intl/hu/policies/terms/" target="_blank">Általános Szerződési Feltételek</a>
-            <br>E-mail: webshop@webshop.com<br>Phone number:+36/1234567<br>Address: 3300, Eger, Érsek utca 9
+<footer class="page-footer font-small pt-4 bg-light border border-success rounded">
+<div class="container-fluid text-center text-md-left">
+    <div class="row">
+<div class="col-md-12 py-5 icons">
+    <div class="mb-5 flex-center">
+      <a class="fb-ic">
+        <i class="fa fa-facebook-f fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
+      </a>
+      <a class="tw-ic">
+        <i class="fa fa-twitter fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
+      </a>
+      <a class="ins-ic">
+        <i class="fa fa-instagram fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
+      </a>
+      <a class="ytb-yt">
+        <i class="fa fa-youtube fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
+      </a>
+    </div>
+</div>
+</div>
+    <div class="contact">
+        <p>E-mail: webshop@webshop.com<br>Phone number:+36/1234567 <br> Address: 3300, Eger, Érsek utca 9</p>
+    </div>
+    <div class="col-md-5 mt-md-0 mt-3">
+        <a href="https://www.google.com/intl/hu/policies/privacy/" target="_blank">Adatvédelem</a><span aria-hidden="true" role="presentation"> - </span><a href="https://www.google.com/intl/hu/policies/terms/" target="_blank">Általános Szerződési Feltételek</a>
+    </div>
         </div>
-    </footer>
+            </div>
 
+<div class="copyright text-center py-3 border border-success rounded">Copyright &copy; Webshop <?= date("Y") ?></div>
+
+</footer>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 </body>
 
 </html>
