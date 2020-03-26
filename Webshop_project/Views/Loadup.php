d3c7b0d980bc8ed385362e65a4e9466d5d11d992
adamg94 <adamgulyas@outlook.com>
2020-03-26 14:02:27 +0100

    Loadup.php view fix

--- Webshop_project/Views/Loadup.php
+++ Webshop_project/Views/Loadup.php
@@ -5,11 +5,11 @@
                 <form class="form-container" action="<?php Loadup::_Loadup() ?>" method="post">
                     <div class="form-group">
                         <label class="tx" for="Image">Image</label>
-                        <input id="Image" type="file" name="Image" class="form-control" required="required">
+                        <input id="Image" type="file" name="ProductImage" class="form-control" required="required">
                     </div>
                     <div class="form-group">
                         <label class="tx" for="Category">Category</label>
-                        <select name="Category" id="Category">
+                        <select name="ProductCategory[]" id="Category">
                             <?php for($i = 1; $i <= Loadup::GetCountOfCategory(); ++$i):?>
                                 <option value="<?= $i ?>"><?= Loadup::GetNameOfCategoryById($i) ?></option>
                             <?php endfor ?>
@@ -21,15 +21,15 @@
                     </div>
                     <div class="form-group">
                         <label class="tx" for="Price">Price</label>
-                        <input id="Price" type="number" name="Price" class="form-control" required="required">
+                        <input id="Price" type="number" name="ProductPrice" class="form-control" required="required">
                     </div>
                     <div class="form-group">
-                        <label class="tx" for="Pieces">Pieces</label>
-                        <input id="Pieces" type="number" name="Pieces" class="form-control" required="required">
+                        <label class="tx" for="Pieces">Quantity</label>
+                        <input id="Quantity" type="number" name="ProductQuantity" class="form-control" required="required">
                     </div>
                     <div class="form-group">
                         <label class="tx" for="Description">Description</label>
-                        <input id="Desctiption" type="text" name="Description" class="form-control" required="required">
+                        <input id="Desctiption" type="text" name="ProductDesctiption" class="form-control" required="required">
                     </div>
                     <button type="submit" class="btn btn-warning btn-block">Loadup</button>
                 </form>
