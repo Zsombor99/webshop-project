64e1d345f1e482a0c5dd4336ba6c71a40b5d00d7
adamg94 <adamgulyas@outlook.com>
2020-03-26 19:24:50 +0100

    H

--- Webshop_project/Views/Error404.php
+++ Webshop_project/Views/Error404.php
@@ -2,5 +2,5 @@
         <h1>404</h1>
         <h2>Error! Page not found.</h2>    
         <p>This is not what are you looking for...</p>
-        <h3><a class="text-decoration-none" href='<?= explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>home'>Return to the home page</a></h3>
+        <h3><a class="text-decoration-none" href='<?= explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>Home'>Return to the home page</a></h3>
     </div>
