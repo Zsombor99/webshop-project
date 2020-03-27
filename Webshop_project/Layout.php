6e147fab499c2997b37752d1117626eb7d7d719e
adamg94 <adamgulyas@outlook.com>
2020-03-27 15:35:16 +0100

    css/main.css Header and footer fixed problems. Slightly changed the design. Bootstrap css removed due to own css props. HTML markup a little bit faster.
    !!
    Main content css still bad, temporary disabled the other own css props.
    !!
    Also header and footer is now incompatible with mobile devices (fix. soon)

--- Webshop_project/Layout.php
+++ Webshop_project/Layout.php
@@ -12,62 +12,58 @@
     </head>
     <body>
         <header>
-            <nav class="bg-light fixed-top">
-                <div class="d-flex flex-row align-items-center">
-                    <img src="assets\pictures\logo.jpg" alt="Webshop logo" width="10%" height="10%">
-                    <a class="btn btn-success" href='<?=explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>Home'><i class="fa fa-home"></i> Home</a>
-                    <a class="btn btn-success ml-2" href='<?=explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>ContactUs'><i class="fa fa-address-book"></i> Contact</a>
-                    <a class="btn btn-success ml-2" href='<?=explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>AboutUs'><i class="fa fa-info"></i> About</a>
+                <div id="logo">
+                    <img src="assets\pictures\logo.png" alt="Webshop logo">
+				</div>
+				<nav>
+                    <a class="button" href='<?=explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>Home'><i class="fa fa-home"></i>Home</a>
+                    <a class="button" href='<?=explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>ContactUs'><i class="fa fa-address-book"></i>Contact</a>
+                    <a class="button" href='<?=explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>AboutUs'><i class="fa fa-info"></i>About</a>
                     <?php if((isset($_SESSION["UserLogged"])) && ($_SESSION["UserLogged"] === True)): ?>
-                    <a class="btn btn-success ml-2" href='<?=explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>Profile'><i class="fa fa-user"></i></i> Profile</a>
-                    <a class="btn btn-success ml-2" href='<?=explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>LoadUp'><i class="fa fa-upload"></i> Loadup</a>
-                    <form action="<?php Logout::_Logout() ?>" method="POST" class="d-flex ml-auto mr-2">
-                        <button type="submit" class="btn btn-success text-white ml-2"><i class="fa fa-sign-out"></i> Logout</button>
+                    <a class="button" href='<?=explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>Profile'><i class="fa fa-user"></i></i>Profile</a>
+                    <a class="button" href='<?=explode("index", preg_quote($_SERVER['PHP_SELF']))[0]?>LoadUp'><i class="fa fa-upload"></i>Loadup</a>
+                    <form action="<?php Logout::_Logout() ?>" method="POST">
+                        <button type="submit"><i class="fa fa-sign-out"></i>Logout</button>
                     </form>
                     <?php else: ?>
-                    <a class="btn btn-success ml-2" href='<?= explode("index", preg_quote($_SERVER['PHP_SELF']))[0] ?>Register'><i class="fa fa-user-plus"></i> Register</a>
-                    <form action="<?php Login::_Login() ?>" method="POST" class="d-flex ml-auto mr-2">
-                        <input class="ml-2 border border-success rounded" id="EmailAddress" type="email" name="EmailAddress" placeholder="Email address" required="required">
-                        <input class="ml-2 border border-success rounded" id="Password" type="password" name="Password" placeholder="Password" required="required">
-                        <button type="submit" class="btn btn-success ml-2">Login</button>
-                    </form>
-                    <?php endif ?>
-                </div>
-            </nav>
+                    <a class="button" href='<?= explode("index", preg_quote($_SERVER['PHP_SELF']))[0] ?>Register'><i class="fa fa-user-plus"></i>Register</a>
+                	<div id="loginarea">
+						<form action="<?php Login::_Login() ?>" method="POST">
+							<input id="EmailAddress" type="email" name="EmailAddress" placeholder="Email address" required="required">
+							<input id="Password" type="password" name="Password" placeholder="Password" required="required">
+							<button class="button" type="submit">Login</button>
+						</form>
+					</div>
+					<?php endif ?>
+				</nav>               
         </header>
-        <main class="container mx-2 w-100">
+        <main>
             <?php include($Content) ?>
         </main>
-        <footer class="page-footer font-small pt-4 bg-light border border-success rounded">
-            <div class="container-fluid text-center text-md-left">
-                <div class="row">
-                    <div class="col-md-12 py-5 icons">
-                        <div class="mb-5 flex-center">
-                            <a class="fb-ic">
-                            <i class="fa fa-facebook-f fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
-                            </a>
-                            <a class="tw-ic">
-                            <i class="fa fa-twitter fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
-                            </a>
-                            <a class="ins-ic">
-                            <i class="fa fa-instagram fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
-                            </a>
-                            <a class="ytb-yt">
-                            <i class="fa fa-youtube fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
-                            </a>
-                        </div>
-                    </div>
+        <footer>
+            <div>
+                <div id="social-icons">
+                    <a class="fb-ic">
+                        <i class="fa fa-facebook-f fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
+                    </a>
+                    <a class="tw-ic">
+                        <i class="fa fa-twitter fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
+                    </a>
+                    <a class="ins-ic">
+                        <i class="fa fa-instagram fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
+                    </a>
+                    <a class="ytb-yt">
+                        <i class="fa fa-youtube fa-lg white-text mr-md-4 mr-3 fa-2x"> </i>
+                    </a>
                 </div>
-                <div class="contact">
+                <div id="contact-info">
                     <p>E-mail: webshop@webshop.com<br>Phone number:+36/1234567 <br> Address: 3300, Eger, Érsek utca 9</p>
-                </div>
-                <div class="col-md-5 mt-md-0 mt-3">
                     <a href="https://www.google.com/intl/hu/policies/privacy/" target="_blank">Adatvédelem</a>
                     <span aria-hidden="true" role="presentation"> - </span>
                     <a href="https://www.google.com/intl/hu/policies/terms/" target="_blank">Általános Szerződési Feltételek</a>
                 </div>
             </div>
-            <div class="copyright text-center py-3 border border-success rounded">Copyright &copy; Webshop <?= date("Y") ?></div>
+            <div id="copyright">Copyright &copy; Webshop <?= date("Y") ?></div>
         </footer>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
