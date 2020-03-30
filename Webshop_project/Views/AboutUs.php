d2ad36f034e71a1d6aceee9a88b325d08e416750
ungererik <ungererik11@gmail.com>
2020-03-30 12:13:56 +0200

    writing about us page text

--- Webshop_project/Views/AboutUs.php
+++ Webshop_project/Views/AboutUs.php
@@ -1,2 +1,6 @@
     <h1 class="heading-h1">About Us</h1>
-    <h2 class="heading-h2">This company has been blah blah blah...</h2>
+    <h2 class="heading-h2">We made this company to make shopping easier from home.</h2>
+    <h2 class="heading-h2">Our products is high quality and we have the best prices for you!</h2>
+    <h2 class="heading-h2">If you order over $1000, we will deliver it to you for free!</h2>
+    <h2 class="heading-h2">Delivery time is 2-3 business days.</h2>
+    
