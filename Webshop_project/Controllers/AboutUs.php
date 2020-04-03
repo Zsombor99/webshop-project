8db4b5d9e2aaeee1174db17fa800d963d1ffb1a3
Zsombor99 <55382782+Zsombor99@users.noreply.github.com>
2020-04-03 18:00:34 +0200

    Controller AboutUs
    működőképes

--- Webshop_project/Controllers/AboutUs.php
+++ Webshop_project/Controllers/AboutUs.php
@@ -1,8 +1,8 @@
 <?php
 class AboutUs extends Controller
 {
-    public static $Title = "About us";
-    public static function Test() {       
-        print_r(self::Query("SELECT * FROM `User`"));
+    public function index(){
+        $this->load_view('AboutUs');
     }
+
 }
