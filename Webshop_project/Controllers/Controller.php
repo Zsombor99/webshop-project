55faf92431184f5d729fef56ccce40c46e2051e5
Zsombor99 <55382782+Zsombor99@users.noreply.github.com>
2020-04-03 18:01:04 +0200

    Delete Old Controller

--- Webshop_project/Controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Controller extends Database
-{
-    public static function CreateView($ViewName) {
-        $Title = $ViewName::$Title;
-        $Content = "./Views/$ViewName.php";
-        include ('./Layout.php');
-    }
-}
